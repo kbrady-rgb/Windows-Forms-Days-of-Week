aaf4c8866c5ac6aa65b247c624a93906edace5c2
kbrady-rgb <58741813+kbrady-rgb@users.noreply.github.com>
2020-09-15 15:32:28 -0500

    Completed project

---
+++ DaysOfTheWeekBrady/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace DaysOfTheWeekBrady
+{
+    partial class DaysOfTheWeekForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblInstructions = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblInstructions
+            // 
+            this.lblInstructions.AutoSize = true;
+            this.lblInstructions.Location = new System.Drawing.Point(16, 15);
+            this.lblInstructions.Name = "lblInstructions";
+            this.lblInstructions.Size = new System.Drawing.Size(376, 17);
+            this.lblInstructions.TabIndex = 0;
+            this.lblInstructions.Text = "Enter a number 1-7 to see corresponding day of the week.";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(137, 51);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 22);
+            this.txtInput.TabIndex = 1;
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblOutput.Location = new System.Drawing.Point(157, 94);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(62, 19);
+            this.lblOutput.TabIndex = 2;
+            this.lblOutput.Text = "             ";
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(81, 135);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 3;
+            this.btnSubmit.Text = "&Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.BtnSubmit_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Location = new System.Drawing.Point(218, 135);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "&Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.BtnExit_Click);
+            // 
+            // DaysOfTheWeekForm
+            // 
+            this.AcceptButton = this.btnSubmit;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(413, 209);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.lblInstructions);
+            this.Name = "DaysOfTheWeekForm";
+            this.Text = "Days of the Week";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblInstructions;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ DaysOfTheWeekBrady/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/***************************************************************
+* Name        : Days of the Week
+* Author      : Kabrina Brady
+* Created     : 9/15/2019
+* Course      : CIS 169 - C#
+* Version     : 1.0
+* OS          : Windows 10
+* Copyright   : This is my own original work
+* Description : User enters a number between 1-7 and corresponding day of the week is displayed (i.e. 1=Monday)
+*               Input:  Clicks, and what user enters into TextBox
+*               Output: Day of the week
+* Academic Honesty: I attest that this is my original work.
+* I have not used unauthorized source code, either modified or unmodified. I have not given other fellow student(s) access to my program.         
+***************************************************************/
+
+
+namespace DaysOfTheWeekBrady
+{
+    public partial class DaysOfTheWeekForm : Form
+    {
+        public DaysOfTheWeekForm()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSubmit_Click(object sender, EventArgs e)
+        {
+                int number; //variable that will hold TextBox input
+                if (int.TryParse(txtInput.Text, out number)) //converts string to int if int entered
+                {
+                    if (number >= 1 && number <= 7) //ensures number is between 1 and 7
+                    {
+                        switch (number)
+                        {
+                        //if number is 1, text = Monday. If 2, Tuesday, etc.
+                            case 1: 
+                                lblOutput.Text = "Monday";
+                                break;
+
+                            case 2:
+                                lblOutput.Text = "Tuesday";
+                                break;
+
+                            case 3:
+                                lblOutput.Text = "Wednesday";
+                                break;
+
+                            case 4:
+                                lblOutput.Text = "Thursday";
+                                break;
+
+                            case 5:
+                                lblOutput.Text = "Friday";
+                                break;
+
+                            case 6:
+                                lblOutput.Text = "Saturday";
+                                break;
+
+                            case 7:
+                                lblOutput.Text = "Sunday";
+                                break;
+                        }
+                    }
+                }
+            if (!(number >= 1 && number <= 7))
+            {
+                MessageBox.Show("Enter a number between 1 and 7."); //tells user to enter a number if TextBox contains something it's not supposed to, like a string
+            }
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            this.Close(); //closes the form
+        }
+    }
+}
